5de1aec049445a06ef3e7f7848cb8ba71331abfb
mingxikay <emoxuanlv39@hotmail.com>
2017-03-07 00:42:23 +0800

    作业1

---
+++ MyApplication/app/src/main/java/study/mingxi/kay/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package study.mingxi.kay.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
